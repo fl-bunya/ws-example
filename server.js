8ba91319158b25e8d75b89c7336a13f6c6ee8f75
fl-bunya <yuichi.bunya@firstlogic.co.jp>
2024-09-05 18:04:58 +0900

    chore: Update package name and improve chat UI

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-// server.js
 const WebSocket = require('ws');
 
 // WebSocketサーバーをポート8080で作成
 const wss = new WebSocket.Server({ port: 8080 });
 
+// メッセージ履歴を保存する配列（最大10件）
+const messageHistory = [];
+
+const MAX_HISTORY = 1000;
+
 wss.on('connection', (ws) => {
   console.log('クライアントが接続しました。');
+
+  // 過去の10件のメッセージを接続したクライアントに送信
+  messageHistory.forEach((message) => {
+    ws.send(message);
+  });
 
   // 接続している全てのクライアントにメッセージを送信する関数
   const broadcast = (data) => {
@@ -19,8 +28,18 @@ wss.on('connection', (ws) => {
   // クライアントからメッセージを受け取ったときの処理
   ws.on('message', (message) => {
     console.log('受信したメッセージ: ', message);
+
+    // メッセージをテキスト形式で履歴に追加
+    const textMessage = message.toString(); // バッファを文字列に変換
+    messageHistory.push(textMessage);
+
+    // メッセージ履歴が10件を超えたら古いものを削除
+    if (messageHistory.length > MAX_HISTORY) {
+      messageHistory.shift(); // 最古のメッセージを削除
+    }
+
     // 全てのクライアントにメッセージを送信
-    broadcast(message);
+    broadcast(textMessage);
   });
 
   // 接続終了時
